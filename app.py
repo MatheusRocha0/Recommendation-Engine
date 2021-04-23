2f4f7948b8c5a2990addb5dfe6860ff8475034fa
Matheus Rocha Salvador Da Silva <mrmatheus34@gmail.com>
2021-04-23 16:56:07 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+
+from flask import Flask, request
+import pickle
+import pandas as pd
+
+engine = pickle.load(open("model.pkl", "rb"))
+
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET", "POST"])
+def predict():
+  test_json = request.get_json()
+
+  if test_json:
+     df_raw = pd.DataFrame(test_json, columns = test_json[0].keys())
+     preds = []
+     col1 = df_raw.columns[0]
+     col2 = df_raw.columns[1]
+     for i in range(len(df_raw)):
+        pred = engine.predict(df_raw[col1][i], df_raw[col2][i]).est
+        preds.append(pred)
+
+  df_raw["user_rating"] = preds
+  return df_raw.to_json(orient = "records")
+
+if __name__ == "__main__":
+  app.run()
